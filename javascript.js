ae3e3a1abb08b7c625442460d565b6f6228d79b7
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-06-05 02:16:38 -0400

    final version!

--- javascript.js
+++ javascript.js
@@ -17,15 +17,13 @@ function clearNode(parentNode) {
     }
 }
 
-function createGrid() {
+function createGrid(answer) {
+
+    basisPercent = `${100/answer}%`;
+
+    console.log(answer);
     
-    do {
-
-        answer = prompt('Please enter a number up to 100','');
-
-    } while (!(answer > 0 && answer <= 100));
-
-    num = answer * answer;
+    num = +answer * +answer
 
     clearNode(container);
 
@@ -36,9 +34,35 @@ function createGrid() {
         divi.setAttribute('id',`${divName}`);
         divi.setAttribute('class', 'grid');
         divi.style.opacity = '1.0';
+        divi.style.flexBasis = `${basisPercent}`;
         container.appendChild(divi);
     };
 
+       divs = document.querySelectorAll('.grid');
+
+    divs.forEach((div) => {
+
+        div.addEventListener('mouseenter', () => {
+
+            // getComputedStyle is necessary to return current value of opacity
+            // storing value of object.style.opacity will give result null
+            // the reason is that it only provides current value of opacity that was set in-line
+
+            computedStyle = getComputedStyle(div);
+
+            currentOpacity = Number(computedStyle.opacity);
+
+            newOpacity = `${currentOpacity - 0.1}`;
+
+            console.log(newOpacity);      
+
+            div.style.cssText = `background: ${randomRGB()}; flex-basis: ${basisPercent};`
+
+            div.style.opacity = newOpacity
+
+            
+        });
+    });
 };
 
 function randomValue() {
@@ -60,72 +84,16 @@ function randomRGB() {
 
 btn.addEventListener('click', () => {
 
-    createGrid();
+    do {
 
-    divs = document.querySelectorAll('.grid');
+    value = prompt('Please enter a number up to 100');
 
-    divs.forEach((div) => {
+    } while (!(value > 0 && value <= 100))
 
-        basisPercent = `${100/answer}%`;
-        
-        div.style.flexBasis = `${basisPercent}`;
-    
-        div.addEventListener('mouseenter', () => {
-        
-            if (div.id != 'btnDiv' && div.id != 'container') {
+    createGrid(value);
 
-                // getComputedStyle is necessary to return current value of opacity
-                // storing value of object.style.opacity will give result null
-                // the reason is that it only provides current value of opacity that was set in-line
-
-                computedStyle = getComputedStyle(div);
-
-                currentOpacity = Number(computedStyle.opacity);
-
-                newOpacity = `${currentOpacity - 0.1}`;
-
-                console.log(newOpacity);      
-
-                div.style.cssText = `background: ${randomRGB()}; flex-basis: ${basisPercent};`
-
-                div.style.opacity = newOpacity
-
-            }
-        });
-    });
 });
 
 // button functionality above
 
-// 16x16 start below
-
-function createStart() {
-
-    for (i = 1; i <= 256; i++) {
-        divi = 'div'+i;
-        divName = divi
-        divi = document.createElement('div');
-        divi.setAttribute('id',`${divName}`);
-        divi.setAttribute('class', 'start');
-        divi.style.flexBasis = '6.25%';
-        container.appendChild(divi);
-    }
-
-    myDivs = document.querySelectorAll('.start');
-
-    myDivs.forEach((myDiv) => {
-
-        myDiv.addEventListener('mouseenter', () => {
-
-            myDiv.style.backgroundColor = "deeppink";
-
-        });
-
-    });
-
-};
-
-createStart();
-
-// 16x16 start above
-
+createGrid(answer=16);
